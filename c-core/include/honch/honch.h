b740f800dce1dc28a150b8a2ebf462681f4f8400
dorocha <me@dorocha.dev>
2026-05-07 06:21:08 +0100

    test(c-core): cover gzip transport payloads

--- c-core/include/honch/honch.h
+++ c-core/include/honch/honch.h
@@ -62,6 +62,7 @@ typedef honch_status_t (*honch_test_transport_fn)(
     long *http_status);
 
 void honch_test_set_transport(honch_test_transport_fn transport, void *userdata);
+honch_status_t honch_test_gzip_payload(const char *payload, unsigned char **out, size_t *out_size);
 #endif
 
 #ifdef __cplusplus
