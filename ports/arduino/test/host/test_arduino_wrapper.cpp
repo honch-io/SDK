f02c30b38fe893bb290e96bd126b3f52af119640
dorocha <me@dorocha.dev>
2026-05-22 04:12:39 +0100

    feat(arduino): persist queue in preferences

--- ports/arduino/test/host/test_arduino_wrapper.cpp
+++ ports/arduino/test/host/test_arduino_wrapper.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "../../src/Honch.h"
+#include "../../src/honch_arduino_adapter.h"
 
 int main() {
   static uint8_t buffer[8192];
@@ -27,6 +28,18 @@ int main() {
   assert(Honch.flush());
   assert(strlen(Honch.deviceId()) > 0);
   assert(strcmp(Honch.lastError(), "ok") == 0);
+  assert(Honch.shutdown());
+
+  honch_arduino_host_transport_reset();
+  honch_arduino_host_transport_set_result(HONCH_ERROR_TRANSPORT, HONCH_TRANSPORT_RETRY);
+  assert(Honch.begin(config));
+  assert(Honch.track("queued", "{}"));
+  assert(!Honch.shutdown());
+
+  honch_arduino_host_transport_set_result(HONCH_OK, HONCH_TRANSPORT_ACCEPTED);
+  assert(Honch.begin(config));
+  assert(Honch.flush());
+  assert(honch_arduino_host_transport_call_count() > 0);
   assert(Honch.shutdown());
   return 0;
 }
