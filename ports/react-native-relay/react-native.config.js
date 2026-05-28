2199634777cb6a13cd4c2bbce7435308829a8301
dorocha <me@dorocha.dev>
2026-05-28 22:11:57 +0100

    feat(react-native-relay): add autolink metadata

--- ports/react-native-relay/react-native.config.js
+++ ports/react-native-relay/react-native.config.js
@@ -2,7 +2,9 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        sourceDir: "./android"
+        sourceDir: "./android",
+        packageImportPath: "import io.honch.reactnativerelay.HonchReactNativeRelayPackage;",
+        packageInstance: "new HonchReactNativeRelayPackage()"
       },
       ios: {
         podspecPath: "./ios/HonchReactNativeRelay.podspec"
