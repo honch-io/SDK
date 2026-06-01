91a5470ef9d1a7e85746c6c843dba13a165effba
dorocha <me@dorocha.dev>
2026-06-02 00:30:05 +0100

    fix(micropython): avoid overriding firmware heap size

--- ports/micropython/usermod/honch/micropython.cmake
+++ ports/micropython/usermod/honch/micropython.cmake
@@ -1,7 +1,3 @@
-if(NOT MICROPY_C_HEAP_SIZE)
-    set(MICROPY_C_HEAP_SIZE 65536)
-endif()
-
 add_library(honch_micropython INTERFACE) # MicroPython usermod INTERFACE_LIBRARY
 
 target_sources(honch_micropython INTERFACE
@@ -28,7 +24,6 @@ target_include_directories(honch_micropython INTERFACE
 
 target_compile_definitions(honch_micropython INTERFACE
     MICROPY_PY_HONCH_CORE=1
-    MICROPY_C_HEAP_SIZE=65536
 )
 
 target_link_libraries(usermod INTERFACE honch_micropython)
