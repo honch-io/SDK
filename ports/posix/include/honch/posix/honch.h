603be6dfe6aaf4a21963da3592960869510c1fe4
dorocha <me@dorocha.dev>
2026-06-02 02:53:41 +0100

    fix(posix): reuse curl easy handle

--- ports/posix/include/honch/posix/honch.h
+++ ports/posix/include/honch/posix/honch.h
@@ -17,6 +17,7 @@ typedef struct honch_posix_storage {
 
 typedef struct honch_posix_transport {
     honch_client_t *client;
+    void *curl;
 } honch_posix_transport_t;
 
 honch_status_t honch_posix_platform_ops_init(honch_platform_ops_t *ops, honch_posix_platform_t *ctx);
@@ -26,6 +27,7 @@ honch_status_t honch_posix_storage_ops_init(
     honch_posix_storage_t *ctx,
     const char *queue_directory);
 honch_status_t honch_posix_transport_ops_init(honch_transport_ops_t *ops, honch_posix_transport_t *ctx);
+void honch_posix_transport_ops_deinit(honch_posix_transport_t *ctx);
 honch_status_t honch_posix_transport_post_chunk(
     honch_client_t *client,
     const char *stream_id,
